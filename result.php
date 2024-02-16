8f8b7cd403bde0393545ddc1b2512d50b22b04b3
rreeggeenntt4 <rreeggeenntt4@yandex.ru>
2024-02-17 02:25:58 +0300

    ready

--- result.php
+++ result.php
@@ -1,44 +1,54 @@
 <?php
 class ChatNotificationManager {
-    function notify($message);
+    public function notify($message) {
+        // Реализация уведомлений чата...
+    }
 }
 
 class ChatMessageRepository {
-    // ...
-    public function save($message)
-    {
-        // ...
+    public function save($message) {
+        // Реализация сохранения сообщения в репозитории...
     }
 }
 
 class ChatMessage {
     private ?int $id;
-    private int $chatld;
-    private Userlnterface $user;
+    private int $chatId;
+    private UserInterface $user;
     private string $text;
-    // ...
-    public function  __construct($chatld, $user, $text) {
-        $this->chatld = $chatld;
+
+    public function __construct($chatId, $user, $text) {
+        $this->chatId = $chatId;
         $this->user = $user;
         $this->text = $text;
-        $chatNotificationManager = new ChatNotificationManager();
-        $chatNotificationManager->notify($message);
     }
 
-    // ...
+    // Геттеры и сеттеры для свойств класса...
+
+    public function sendNotification(ChatNotificationManager $notificationManager) {
+        $notificationManager->notify($this);
+    }
 }
 
-class Chat{
-    public int $id;
-    public ChatMessage $messages;
+class Chat {
+    private int $id;
+    private array $messages;
 
-    // ...
+    public function __construct($id) {
+        $this->id = $id;
+        $this->messages = array();
+    }
+
+    // Геттеры и сеттеры для свойств класса...
 
     public function addMessage($user, $text) {
         $message = new ChatMessage($this->id, $user, $text);
+        $this->messages[] = $message;
+
         $chatMessageRepository = new ChatMessageRepository();
         $chatMessageRepository->save($message);
+
+        $chatNotificationManager = new ChatNotificationManager();
+        $message->sendNotification($chatNotificationManager);
     }
-    
-    // ...
 }
